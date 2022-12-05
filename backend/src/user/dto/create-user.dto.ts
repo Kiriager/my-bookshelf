7e8c1144b45e853294e25e049beacbd38da0185c
Kiriager <kiriager@gmail.com>
2022-12-05 23:25:46 +0200

    add validation pipe

--- backend/src/user/dto/create-user.dto.ts
+++ backend/src/user/dto/create-user.dto.ts
@@ -1,3 +1,11 @@
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+
 export class CreateUserDto {
-  readonly name: string;
+  @IsNotEmpty()
+  @IsEmail()
+  readonly email: string;
+
+  @IsNotEmpty()
+  @IsString()
+  readonly password: string;
 }
