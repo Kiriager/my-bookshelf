ea206af26c80fbe1eb119b97d9c2695f4e5c985f
Kiriager <kiriager@gmail.com>
2022-12-05 23:08:08 +0200

    add string columns

--- backend/src/user/entities/user.entity.ts
+++ backend/src/user/entities/user.entity.ts
@@ -1,8 +1,11 @@
-import { Column, Model, Table } from 'sequelize-typescript';
+import { Column, DataType, Model, Table } from 'sequelize-typescript';
 import { InferAttributes, InferCreationAttributes } from 'sequelize/types';
 
-@Table
+@Table({ tableName: 'users', underscored: true })
 export class User extends Model<InferAttributes<User>, InferCreationAttributes<User>> {
-  @Column
-  public name: string;
+  @Column({ type: DataType.STRING, unique: true, allowNull: false })
+  email: string;
+
+  @Column({ type: DataType.STRING, allowNull: false })
+  passwordHash: string;
 }
