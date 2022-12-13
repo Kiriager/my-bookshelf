460cd24b4354b4ed26880f6cb821f37756e5877b
Kiriager <kiriager@gmail.com>
2022-12-13 12:48:11 +0200

    delete unnecessary parts

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -6,9 +6,6 @@ import TextInput from 'src/ui/common/TextInput/TextInput';
 import PasswordInput from 'src/ui/common/PasswordInput';
 import Button from 'src/ui/common/Button/Button';
 
-// import { useAppSelector, useAppDispatch } from 'src/hooks';
-// import { selectUser, login, userUnauthorize } from 'src/store/slices';
-// import { REGISTER_ROUTE } from 'src/config';
 interface LogInFormValues {
   email: string;
   password: string;
@@ -58,60 +55,6 @@ const LogInPage: React.FC = () => {
       </div>
     </div>
   );
-
-  // return (
-  //   <Formik
-  //     initialValues={{ email: '', password: '' }}
-  //     onSubmit={values => {
-  //       //dispatch(login(values.email, values.password));
-  //       console.log('email' + values.email);
-  //       console.log('password' + values.password);
-  //     }}
-  //     validateOnMount
-  //     validationSchema={Yup.object({
-  //       email: Yup.string().required('Email is required').email('The email is invalid'),
-  //       password: Yup.string().required('Password is required'),
-  //     })}
-  //   >
-  //     {({ isValid }) => (
-  //       <Form
-  //         onChange={() => {
-  //           //if (userState.status === 'unauthorized') dispatch(userUnauthorize());
-  //         }}
-  //       >
-  //         <div className='w-96'>
-  //           <Field
-  //             id='email'
-  //             component={TextInput}
-  //             name='email'
-  //             placeholder={t('auth.emailPlaceholder')}
-  //             label={''}
-  //             // error={userState.error?.type === 'unregistered' ? userState.error?.message : undefined}
-  //           />
-  //           <Field
-  //             id='password'
-  //             name='password'
-  //             component={PasswordInput}
-  //             placeholder={t('auth.passwordPlaceholder')}
-  //             label={''}
-  //             // error={
-  //             //   userState.error?.type === 'invalidPassword' ? userState.error?.message : undefined
-  //             // }
-  //           />
-  //           <Button
-  //             id='submit-registration'
-  //             name='submit-registration'
-  //             onClick={() => undefined}
-  //             //isDisabled={userState.status === 'loggingIn' || !isValid}
-  //             buttonType='submit'
-  //             title={t('auth.signInLabel')}
-  //             format='secondary'
-  //           />
-  //         </div>
-  //       </Form>
-  //     )}
-  //   </Formik>
-  // );
 };
 
 export default LogInPage;
